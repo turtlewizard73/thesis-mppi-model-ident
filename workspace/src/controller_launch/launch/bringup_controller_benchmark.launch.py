57d0e4d3e9f48ae6af9bb1b2cc278b93f055d3ff
turtlewizard73 <mate.laszlo703@gmail.com>
2024-11-04 19:28:41 +0100

    benchmark launch from new pkg done

--- workspace/src/controller_launch/launch/bringup_controller_benchmark.launch.py
+++ workspace/src/controller_launch/launch/bringup_controller_benchmark.launch.py
@@ -19,17 +19,22 @@ def generate_launch_description():
 
     gui = LaunchConfiguration('gui')
 
-    map_file = os.path.join(this_package_dir, '10by10_empty.yaml')
+    map_file = os.path.join(this_package_dir, 'empty_map.yaml')
 
-    nav_config = os.path.join(this_package_dir, 'default_nav2_params.yaml')
+    nav_config = os.path.join(this_package_dir, 'nav2_params_benchmark.yaml')
 
     lifecycle_nodes = ['map_server', 'planner_server', 'controller_server']
     world = os.path.join(this_package_dir, 'empty_world.world')
 
-    urdf = os.path.join(nav2_bringup_dir, 'urdf', 'turtlebot3_waffle.urdf')
+    urdf = os.path.join(this_package_dir, 'turtlebot3_waffle.urdf')
 
     with open(urdf, 'r') as urdf_file:
         robot_description = urdf_file.read()
+
+    robot_name = 'turtlebot3_waffle'
+    robot_sdf = os.path.join(this_package_dir, 'waffle_noiseless.model')
+    pose = {'x': '0.0', 'y': '0.0', 'z': '0.0',
+            'R': '0.0', 'P': '0.0', 'Y': '0.0'}
 
     # -------------------------------------------------------------------------------------
     # Nodes, processes:                         -> task                 - config file
@@ -46,21 +51,54 @@ def generate_launch_description():
     # gazebo_ros/spawn_entity.py                -> spawn robot          - robot_sdf, pose
     # -------------------------------------------------------------------------------------
 
-    robot_name = 'turtlebot3_waffle'
-    robot_sdf = os.path.join(this_package_dir, 'waffle_noiseless.model')
-    pose = {'x': '0.0', 'y': '0.0', 'z': '0.0',
-            'R': '0.0', 'P': '0.0', 'Y': '0.0'}
-
     return LaunchDescription([
-        # DeclareLaunchArgument(
-        #     'config', default_value='nav2_params_mppi.yaml',
-        #     description='Whether to use default nav2 configuration.'
-        # ),
-
         DeclareLaunchArgument(
             'gui', default_value='True',
             description='Whether to run gazebo headless.'
         ),
+
+        # RVIZ
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(nav2_bringup_dir, 'launch', 'rviz_launch.py')),
+            launch_arguments={'namespace': '',
+                              'use_namespace': 'False'}.items()),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(gazebo_dir, 'launch', 'gzserver.launch.py')),
+            launch_arguments={'world': world,
+                              'pause': 'false',
+                              'verbose': 'true'}.items()),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(gazebo_dir, 'launch', 'gzclient.launch.py')),
+            launch_arguments={'verbose': 'true'}.items(),
+            condition=IfCondition(gui)),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': True,
+                        'robot_description': robot_description}],
+            remappings=[('/tf', 'tf'),
+                        ('/tf_static', 'tf_static')]),
+
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            output='screen',
+            arguments=[
+                '-entity', robot_name,
+                '-file', robot_sdf,
+                # '-topic', 'robot_description',
+                '-timeout', '60',
+                '-robot_namespace', '',
+                '-x', pose['x'], '-y', pose['y'], '-z', pose['z'],
+                '-R', pose['R'], '-P', pose['P'], '-Y', pose['Y']]),
 
         Node(
             package='tf2_ros',
@@ -107,59 +145,4 @@ def generate_launch_description():
             parameters=[{'use_sim_time': True},
                         {'autostart': True},
                         {'node_names': lifecycle_nodes}]),
-
-        # RVIZ
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(nav2_bringup_dir, 'launch', 'rviz_launch.py')),
-            launch_arguments={'namespace': '',
-                              'use_namespace': 'False'}.items()),
-
-        # Gazebo simulation
-        # ExecuteProcess(
-        #     cmd=['gzserver', '-s', 'libgazebo_ros_init.so',
-        #          '-s', 'libgazebo_ros_factory.so',
-        #          '-s', 'libgazebo_ros_state.so', world],
-        #     cwd=[nav2_bringup_dir], output='screen'),
-
-        # ExecuteProcess(
-        #     condition=IfCondition(str(use_gui)),
-        #     cmd=['gzclient'],
-        #     cwd=[nav2_bringup_dir], output='screen'),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(gazebo_dir, 'launch', 'gzserver.launch.py')),
-            launch_arguments={'world': world,
-                              'pause': 'false',
-                              'verbose': 'true'}.items()),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(gazebo_dir, 'launch', 'gzclient.launch.py')),
-            launch_arguments={'verbose': 'true'}.items(),
-            condition=IfCondition(gui)),
-
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': True,
-                        'robot_description': robot_description}],
-            remappings=[('/tf', 'tf'),
-                        ('/tf_static', 'tf_static')]),
-
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            output='screen',
-            arguments=[
-                '-entity', robot_name,
-                '-file', robot_sdf,
-                # '-topic', 'robot_description',
-                '-timeout', '60',
-                '-robot_namespace', '',
-                '-x', pose['x'], '-y', pose['y'], '-z', pose['z'],
-                '-R', pose['R'], '-P', pose['P'], '-Y', pose['Y']])
     ])
